d9c21071f1ef2dc1718c6d7c4e877e2d3a05e2fc
Peter Yu <peteryu@gmail.com>
2021-09-24 22:04:38 +0000

    dino

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [ dinoQuote , setDinoQuote ] = useState('');
+
+  const DINO_QUERY = `
+  {
+    ethereum(network: matic) {
+      dexTrades(
+        baseCurrency: {is: "0xAa9654BECca45B5BDFA5ac646c939C62b527D394"}
+        quoteCurrency: {is: "0x2791bca1f2de4661ed88a30c99a7a9449aa84174"}
+        options: {desc: ["block.height"], limit: 1}
+      ) {
+        block {
+          height
+          timestamp {
+            time(format: "%Y-%m-%d %H:%M:%S")
+          }
+        }
+        baseCurrency {
+          symbol
+        }
+        quoteCurrency {
+          symbol
+        }
+        quotePrice
+      }
+    }
+  }
+  `
+
+  useEffect(()=> {
+    async function dinoQuery() {
+      const res = await fetch('https://graphql.bitquery.io', {
+        method: "POST",
+        headers: { "Content-Type": "application/json"},
+        body: JSON.stringify({ query: DINO_QUERY})
+      });
+      const jsonBody = await res.json();
+      console.log(jsonBody.data.ethereum.dexTrades[0].baseCurrency.symbol);
+    }
+    dinoQuery((quotePrice) => setDinoQuote(quotePrice));
+  },[]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>The quote price is: {dinoQuote}</h1>
     </div>
   );
 }
