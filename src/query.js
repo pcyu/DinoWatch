b6f9752648ff06ce192e189558335c74d7570447
Peter Yu <peteryu@gmail.com>
2021-09-28 21:50:19 +0000

    added additional graphql queries with other potential tokens i might want to keep track of

--- src/query.js
+++ src/query.js
@@ -3,8 +3,8 @@ const DINO_QUERY = `
     ethereum(network: matic) {
       dexTrades(
         baseCurrency: {is: "0xAa9654BECca45B5BDFA5ac646c939C62b527D394"}
-        quoteCurrency: {is: "0x7ceb23fd6bc0add59e62ac25578270cff1b9f619"}
-        options: {desc: ["block.height"], limit: 1}
+        quoteCurrency: {is: "0x2791bca1f2de4661ed88a30c99a7a9449aa84174"}
+        options: {desc: ["block.height", "transaction.index"], limit: 1}
       ) {
         block {
           height
@@ -12,16 +12,83 @@ const DINO_QUERY = `
             time(format: "%Y-%m-%d %H:%M:%S")
           }
         }
+        transaction {
+          index
+        }
         baseCurrency {
           symbol
+          name
         }
         quoteCurrency {
           symbol
+          name
         }
         quotePrice
       }
     }
   }
-  `
+`
+
+// const DG_QUERY = `
+//   {
+//     ethereum(network: matic) {
+//       dexTrades(
+//         baseCurrency: {is: "0x2a93172c8dccbfbc60a39d56183b7279a2f647b4"}
+//         quoteCurrency: {is: "0x2791bca1f2de4661ed88a30c99a7a9449aa84174"}
+//         options: {desc: ["block.height", "transaction.index"], limit: 1}
+//       ) {
+//         block {
+//           height
+//           timestamp {
+//             time(format: "%Y-%m-%d %H:%M:%S")
+//           }
+//         }
+//         transaction {
+//           index
+//         }
+//         baseCurrency {
+//           symbol
+//           name
+//         }
+//         quoteCurrency {
+//           symbol
+//           name
+//         }
+//         quotePrice
+//       }
+//     }
+//   }
+// `
+
+// const WORK_QUERY = `
+//   {
+//     ethereum(network: matic) {
+//       dexTrades(
+//         baseCurrency: {is: "0x6002410dda2fb88b4d0dc3c1d562f7761191ea80"}
+//         quoteCurrency: {is: "0x2791bca1f2de4661ed88a30c99a7a9449aa84174"}
+//         options: {desc: ["block.height", "transaction.index"], limit: 1}
+//       ) {
+//         block {
+//           height
+//           timestamp {
+//             time(format: "%Y-%m-%d %H:%M:%S")
+//           }
+//         }
+//         transaction {
+//           index
+//         }
+//         baseCurrency {
+//           symbol
+//           name
+//         }
+//         quoteCurrency {
+//           symbol
+//           name
+//         }
+//         quotePrice
+//       }
+//     }
+//   }
+// `
 
   export default DINO_QUERY
