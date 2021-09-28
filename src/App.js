dc6654b15a3c582507aee157568757230d64b54a
Peter Yu <peteryu@gmail.com>
2021-09-28 22:45:42 +0000

    added multiple graphql calls in query.js.  also fetched the new queries and output additional quote prices into the UI.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
-import DINO_QUERY from '../src/query';
+import { DINO_QUERY, DG_QUERY, WORK_QUERY } from '../src/query';
 import { useState, useEffect } from 'react';
 
 function App() {
-  const [ dinoQuote , setDinoQuote ] = useState('loading');
+  
+  const [ quoteDino , setDinoQuote ] = useState('loading');
+  const [ quoteDG , setDGQuote ] = useState('loading');
+  const [ quoteWork , setWorkQuote ] = useState('loading');
 
   useEffect(()=> {
-    async function dinoQuery() {
+    async function queryDino() {
       const res = await fetch('https://graphql.bitquery.io', {
         method: "POST",
         headers: { 
@@ -18,13 +21,47 @@ function App() {
       const jsonBody = await res.json();
       setDinoQuote(jsonBody.data.ethereum.dexTrades[0].quotePrice)
     }
-    dinoQuery();
-  },[]);
+    queryDino();
+  },[quoteDino]);
+
+  useEffect(()=> {
+    async function queryDG() {
+      const res = await fetch('https://graphql.bitquery.io', {
+        method: "POST",
+        headers: { 
+          "Content-Type": "application/json",
+          "X-API-KEY": "BQYNuy1t9n3tG1RZf1gOpJTGI7RjRGTp"
+        },
+        body: JSON.stringify({ query: DG_QUERY })
+      });
+      const jsonBody = await res.json();
+      setDGQuote(jsonBody.data.ethereum.dexTrades[0].quotePrice)
+    }
+    queryDG();
+  },[quoteDG]);
+
+  useEffect(()=> {
+    async function queryWork() {
+      const res = await fetch('https://graphql.bitquery.io', {
+        method: "POST",
+        headers: { 
+          "Content-Type": "application/json",
+          "X-API-KEY": "BQYNuy1t9n3tG1RZf1gOpJTGI7RjRGTp"
+        },
+        body: JSON.stringify({ query: WORK_QUERY})
+      });
+      const jsonBody = await res.json();
+      setWorkQuote(jsonBody.data.ethereum.dexTrades[0].quotePrice)
+    }
+    queryWork();
+  },[quoteWork]);
 
 
   return (
     <div className="App">
-      <h1>The quote price is: {dinoQuote}</h1>
+      <h1>The quote price is: {quoteDino}</h1>
+      <h1>The quote price is: {quoteDG}</h1>
+      <h1>The quote price is: {quoteWork}</h1>
     </div>
   );
 }
